e4c6e7ecbd4b67f68a2cbe575d553ced1eddd2d4
Teresa E. Gimeno <tegimeno@gmail.com>
2021-09-29 18:38:09 +0200

    test

--- phlRead/mergeMorpho.R
+++ phlRead/mergeMorpho.R
@@ -11,3 +11,4 @@ morpho_all$Date_i <- ymd(morpho_all$date_i)
 morpho_all$Date_f <- ymd(morpho_all$date_f)
 morpho_all$n_days <- difftime(morpho_all$Date_f, morpho_all$Date_i, 'days')
 write.csv(morpho_all, file ='phlOutput/morpho_all.csv', row.names = F)
+#test
